faa543dd4d920711b410626ee4dd78900fd1be55
黄鑫 <xin.huang@hpe.com>
2018-11-29 11:27:44 +0800

    修改项目结构

---
+++ src/main/java/com/numeral/NumeralCalculationsApplication.java
@@ -0,0 +1,18 @@
+package com.numeral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * NumeralCalculationsApplication
+ *
+ * @author huangxin
+ * @date 2018/11/29 11:09
+ */
+@SpringBootApplication
+public class NumeralCalculationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumeralCalculationsApplication.class, args);
+    }
+}
